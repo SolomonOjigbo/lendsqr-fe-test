2d2f6a223414a947ab7dd047fddaf0b43949e5fb
SolomonOjigbo <solomonojigbo@gmail.com>
2022-10-25 23:00:57 +0100

    Added media queries to make the sidebar and navbar component responsive

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Sidebar.scss";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFileClipboard } from "@fortawesome/free-regular-svg-icons";
+
 import {
 	faBriefcase,
 	faBuildingColumns,
@@ -29,110 +31,112 @@ import {
 
 const Sidebar = () => {
 	return (
-		<div className="sidebar">
-			<div className="top">
-				<div className="organisation">
-					<BusinessCenterIcon fontSize="small" />
-					<h2>Switch Organization</h2>
-					<KeyboardArrowDownIcon />
-				</div>
-				<div className="mainDash">
-					<FontAwesomeIcon icon={faHouseChimney} className="icon" />
+		<div className="sidebar_container">
+			<motion.div className="sidebar">
+				<div className="top">
+					<div className="organisation">
+						<BusinessCenterIcon fontSize="small" />
+						<p>Switch Organization</p>
+						<KeyboardArrowDownIcon className="icon" />
+					</div>
+					<div className="mainDash">
+						<FontAwesomeIcon icon={faHouseChimney} className="icon" />
 
-					<p> Dashboard</p>
+						<p> Dashboard</p>
+					</div>
 				</div>
-			</div>
 
-			<div className="menu">
-				<ul>
-					<p className="title">CUSTOMERS</p>
-					<Link to="/" style={{ textDecoration: "none" }}>
+				<div className="menu">
+					<ul>
+						<p className="title">CUSTOMERS</p>
+						<Link to="/" style={{ textDecoration: "none" }}>
+							<li>
+								<FontAwesomeIcon icon={faUserGroup} className="icon" />
+								<span>Users</span>
+							</li>
+						</Link>
 						<li>
-							<FontAwesomeIcon icon={faUserGroup} className="icon" />
-							<span>Users</span>
+							<FontAwesomeIcon icon={faUsers} className="icon" />
+							<span>Guarantors</span>
 						</li>
-					</Link>
-					<li>
-						<FontAwesomeIcon icon={faUsers} className="icon" />
-						<span>Guarantors</span>
-					</li>
 
-					<li>
-						<FontAwesomeIcon icon={faSackDollar} className="icon" />
-						<span>Loans</span>
-					</li>
-					<li>
-						<FontAwesomeIcon icon={faHandshake} className="icon" />
-						<span>Decision Models</span>
-					</li>
-					<li>
-						<FontAwesomeIcon icon={faPiggyBank} className="icon" />
-						<span>Savings</span>
-					</li>
-					<li>
-						<FontAwesomeIcon icon={faHandHoldingDollar} className="icon" />
-						<span>Loan Requests</span>
-					</li>
-					<li>
-						<FontAwesomeIcon icon={faUserCheck} className="icon" />
-						<span>Whitelist</span>
-					</li>
-					<li>
-						<FontAwesomeIcon icon={faUserXmark} className="icon" />
-						<span>Karma</span>
-					</li>
-					<p className="title">BUSINESSES</p>
-					<li>
-						<FontAwesomeIcon icon={faBriefcase} className="icon" />
-						<span>Organisation</span>
-					</li>
-					<li>
-						<FontAwesomeIcon icon={faHandHoldingDollar} className="icon" />
-						<span>Loan Products</span>
-					</li>
-					<li>
-						<FontAwesomeIcon icon={faBuildingColumns} className="icon" />
-						<span>Savings Products</span>
-					</li>
-					<li>
-						<FontAwesomeIcon icon={faCoins} className="icon" />
-						<span>Fees and Charges</span>
-					</li>
-					<li>
-						<FontAwesomeIcon icon={faMoneyBillTransfer} className="icon" />
-						<span>Transactions</span>
-					</li>
-					<li>
-						<FontAwesomeIcon icon={faFan} className="icon" />
-						<span>Services</span>
-					</li>
-					<li>
-						<FontAwesomeIcon icon={faUserGear} className="icon" />
-						<span>Service Account</span>
-					</li>
-					<li>
-						<FontAwesomeIcon icon={faScroll} className="icon" />
-						<span>Settlements</span>
-					</li>
-					<li>
-						<FontAwesomeIcon icon={faChartColumn} className="icon" />
-						<span>Reports</span>
-					</li>
-					<p className="title">SETTINGS</p>
-					<li>
-						<FontAwesomeIcon icon={faSliders} className="icon" />
-						<span>Preferences</span>
-					</li>
-					<li>
-						<FontAwesomeIcon icon={faFileInvoice} className="icon" />
-						<span>Fees and Pricing</span>
-					</li>
-					<li>
-						<FontAwesomeIcon icon={faFileClipboard} className="icon" />
-						<span>Audit Logs</span>
-					</li>
-				</ul>
-			</div>
+						<li>
+							<FontAwesomeIcon icon={faSackDollar} className="icon" />
+							<span>Loans</span>
+						</li>
+						<li>
+							<FontAwesomeIcon icon={faHandshake} className="icon" />
+							<span>Decision Models</span>
+						</li>
+						<li>
+							<FontAwesomeIcon icon={faPiggyBank} className="icon" />
+							<span>Savings</span>
+						</li>
+						<li>
+							<FontAwesomeIcon icon={faHandHoldingDollar} className="icon" />
+							<span>Loan Requests</span>
+						</li>
+						<li>
+							<FontAwesomeIcon icon={faUserCheck} className="icon" />
+							<span>Whitelist</span>
+						</li>
+						<li>
+							<FontAwesomeIcon icon={faUserXmark} className="icon" />
+							<span>Karma</span>
+						</li>
+						<p className="title">BUSINESSES</p>
+						<li>
+							<FontAwesomeIcon icon={faBriefcase} className="icon" />
+							<span>Organisation</span>
+						</li>
+						<li>
+							<FontAwesomeIcon icon={faHandHoldingDollar} className="icon" />
+							<span>Loan Products</span>
+						</li>
+						<li>
+							<FontAwesomeIcon icon={faBuildingColumns} className="icon" />
+							<span>Savings Products</span>
+						</li>
+						<li>
+							<FontAwesomeIcon icon={faCoins} className="icon" />
+							<span>Fees and Charges</span>
+						</li>
+						<li>
+							<FontAwesomeIcon icon={faMoneyBillTransfer} className="icon" />
+							<span>Transactions</span>
+						</li>
+						<li>
+							<FontAwesomeIcon icon={faFan} className="icon" />
+							<span>Services</span>
+						</li>
+						<li>
+							<FontAwesomeIcon icon={faUserGear} className="icon" />
+							<span>Service Account</span>
+						</li>
+						<li>
+							<FontAwesomeIcon icon={faScroll} className="icon" />
+							<span>Settlements</span>
+						</li>
+						<li>
+							<FontAwesomeIcon icon={faChartColumn} className="icon" />
+							<span>Reports</span>
+						</li>
+						<p className="title">SETTINGS</p>
+						<li>
+							<FontAwesomeIcon icon={faSliders} className="icon" />
+							<span>Preferences</span>
+						</li>
+						<li>
+							<FontAwesomeIcon icon={faFileInvoice} className="icon" />
+							<span>Fees and Pricing</span>
+						</li>
+						<li>
+							<FontAwesomeIcon icon={faFileClipboard} className="icon" />
+							<span>Audit Logs</span>
+						</li>
+					</ul>
+				</div>
+			</motion.div>
 		</div>
 	);
 };
